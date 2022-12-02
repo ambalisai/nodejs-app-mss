c880e7d3b88cf748239648d8d4deefa0c0c567e3
ambalisai <116532359+ambalisai@users.noreply.github.com>
2022-12-02 14:22:03 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.235.100.155:9000/',
+  serverUrl: 'http://172.31.41.180:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
